95a237e404e18d5041a28ab4d13b72e98e46c1cd
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-05-18 22:15:17 +0900

    feat : ingame card data manage #3

--- OpenEnding/Assets/Scripts/Game/GameType/SealGame/Container/SealGame_CardContainer.cs
+++ OpenEnding/Assets/Scripts/Game/GameType/SealGame/Container/SealGame_CardContainer.cs
@@ -1,26 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SealGame_CardContainer : Singleton<SealGame_CardContainer>
+[Serializable]
+public class SealGame_CardContainer
 {
-    public List<SealGame_DeviceData> deviceDataList = new List<SealGame_DeviceData>();
+    public List<SealGame_CardData> deviceDataList = new List<SealGame_CardData>();
 
     public void InitDeviceDataList()
     {
-        // Add Client Device Data
-        for (int i = 0; i < ConnectManager.Instance.connectedDeviceList.Count; i++)
+        for (int i = 0; i < NetworkManager.Instance.connectedDeviceList.Count; i++)
         {
-            deviceDataList.Add(new SealGame_DeviceData(i));
+            deviceDataList.Add(new SealGame_CardData(i));
         }
-        
-        // Add Server Device Data
-        int serverIndex = deviceDataList.Count;
-        deviceDataList.Add(new SealGame_DeviceData(serverIndex));
     }
 
     public void SetDeviceData(int deviceIndex, Define.SealGameCardType targetCardType)
     {
+        DebugText.Instance.AddText($"DeviceList Count : {NetworkManager.Instance.connectedDeviceList.Count}");
+        
         deviceDataList[deviceIndex].cardType = targetCardType;
     }
 }
--- OpenEnding/Assets/Scripts/Game/GameType/SealGame/Data/SealGame_CardData.cs
+++ OpenEnding/Assets/Scripts/Game/GameType/SealGame/Data/SealGame_CardData.cs
@@ -5,22 +5,22 @@ using Shatalmic;
 using UnityEngine;
 
 [Serializable]
-public class SealGame_DeviceData
+public class SealGame_CardData
 {
     public int deviceNum = -1;
     public Define.SealGameCardType cardType = Define.SealGameCardType.None;
     public bool isMine = false;
     public Networking.NetworkDevice networkDevice;
 
-    public SealGame_DeviceData(int deviceNum)
+    public SealGame_CardData(int deviceNum)
     {
         this.deviceNum = deviceNum;
         cardType = Define.SealGameCardType.None;
         isMine = false;
 
-        if (deviceNum < ConnectManager.Instance.connectedDeviceList.Count)
+        if (deviceNum < NetworkManager.Instance.connectedDeviceList.Count)
         {
-            networkDevice = ConnectManager.Instance.connectedDeviceList[deviceNum];    
+            networkDevice = NetworkManager.Instance.connectedDeviceList[deviceNum];    
         }
         else
         {
