c4162d6635754c439978c24a52f2abc1f473bdd2
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-05-18 22:22:25 +0900

    feat : packet data format settings, packet data handling #1 #3

--- OpenEnding/Assets/Scripts/Game/GameType/SealGame/Manager/SealGame_Scene.cs
+++ OpenEnding/Assets/Scripts/Game/GameType/SealGame/Manager/SealGame_Scene.cs
@@ -1,8 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SealGame_Scene : MonoBehaviour
+public class SealGame_Scene : Singleton<SealGame_Scene>
 {
-    
+    [SerializeField]
+    private Image cardImage;
+
+    private void Awake()
+    {
+        switch (NetworkManager.Instance.connectType)
+        {
+            case Define.ConnectType.Server:
+                NetworkManager.Instance.StartServer();
+                break;
+            case Define.ConnectType.Client:
+                NetworkManager.Instance.StartClient();
+                break;
+        }
+    }
+    public void SetCardImageSeal(byte[] bytes)
+    {
+        DebugText.Instance.AddText("물개 카드 !");
+        cardImage.sprite = Resources.Load<Sprite>("Sprites/Seal");
+    }
 }
