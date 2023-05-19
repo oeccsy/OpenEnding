b76939a505b3f43fd71df01968f1db753184b4bb
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-05-19 20:20:24 +0900

    chore : 테스트용 windows build에서 background 상태에서도 화면 업데이트 #2

---
+++ OpenEnding/Assets/Scripts/Game/Manager/LifeCycleManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycleManager : Singleton<LifeCycleManager>
+{
+    private void Awake()
+    {
+#if DEVELOPMENT_BUILD
+#if UNITY_STANDALONE_WIN
+        Application.runInBackground = true;
+#endif
+#endif
+    }
+}
