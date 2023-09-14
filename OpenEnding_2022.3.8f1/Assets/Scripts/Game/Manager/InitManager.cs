a47e207139cfae1c10063fa5d88f6b0e544bf485
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-09-15 04:00:59 +0900

    feat : InitManager로 부하 완화

---
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/InitManager.cs
@@ -0,0 +1,11 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class InitManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        DOTween.Init(false, false, LogBehaviour.ErrorsOnly).SetCapacity(100, 20);
+        ColorPalette.Init();
+    }
+}
