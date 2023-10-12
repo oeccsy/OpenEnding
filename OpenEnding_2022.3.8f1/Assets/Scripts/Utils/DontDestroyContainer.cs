4904dd67eda23d0e4e17c6e7baba18af4c640ac4
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-13 01:03:04 +0900

    feat : DontDestroyContainter.cs 에 singleton 적용

--- OpenEnding_2022.3.8f1/Assets/Scripts/Utils/DontDestroyContainer.cs
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Utils/DontDestroyContainer.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DontDestroyContainer : MonoBehaviour
+public class DontDestroyContainer : Singleton<DontDestroyContainer>
 {
     private void Awake()
     {
