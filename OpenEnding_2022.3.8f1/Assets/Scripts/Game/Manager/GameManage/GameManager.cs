55018d0aee0125a0c6f9ae79818862133dee97ef
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-17 20:59:23 +0900

    feat : GameState

--- OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/GameManage/GameManager.cs
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/GameManage/GameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GameManager : Singleton<GameManager>
 {
     public GameMode GameMode { get; set; }
+    public GameState GameState { get; set; }
     public GameFlow GameFlow { get; set; }
     public PlayerController PlayerController { get; set; }
 }
