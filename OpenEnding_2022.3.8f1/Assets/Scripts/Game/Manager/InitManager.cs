9ee0ee2cf05058f6bd60472fd9f96c814db40e17
YunSeong <77562357+oeccsy@users.noreply.github.com>
2023-10-17 21:17:36 +0900

    feat : InitManager에서 DontDestroyOnLoadContainer 생성

--- OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/InitManager.cs
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/InitManager.cs
@@ -4,17 +4,31 @@ using UnityEngine;
 
 public class InitManager : MonoBehaviour
 {
+    private static bool isInit = false;
     private void Awake()
     {
+        if (isInit) return;
+        
         DOTween.Init(false, false, LogBehaviour.ErrorsOnly).SetCapacity(100, 20);
         ColorPalette.Init();
+
+        var prefab = Resources.Load<GameObject>("Prefabs/DontDestroyOnLoadContainer");
+        Instantiate(prefab);
     }
 
     private IEnumerator Start()
     {
+        if (isInit) yield break;
+        
         yield return new WaitForSecondsRealtime(2f);
         
         var bottomSheet = UIManager.Instance.ShowPopup("Prefabs/OnBoardingBottomSheet");
-        bottomSheet.transform.SetParent(DontDestroyContainer.Instance.transform);
+        bottomSheet.transform.SetParent(DontDestroyOnLoadContainer.Instance.transform);
+
+        isInit = true;
+        
+#if DEVELOPMENT_BUILD || UNITY_EDITOR || UNITY_STANDALONE_WIN
+        DebugCanvas.Instance.InitDebugCanvas();
+#endif
     }
 }
--- OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/LifeCycleManager.cs
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/LifeCycleManager.cs
@@ -8,14 +8,11 @@ public class LifeCycleManager : Singleton<LifeCycleManager>
     protected override void Awake()
     {
         base.Awake();
-        
-#if DEVELOPMENT_BUILD || UNITY_EDITOR || UNITY_STANDALONE_WIN
-        DebugCanvas.Instance.InitDebugCanvas();
-        
+
 #if UNITY_STANDALONE_WIN
         Application.runInBackground = true;
 #endif
-#endif
+
     }
 
     private void OnApplicationFocus(bool hasFocus)
