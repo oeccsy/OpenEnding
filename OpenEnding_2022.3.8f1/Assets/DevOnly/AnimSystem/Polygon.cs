d2b1b03acb89783af09bf4fb4795833fce8e7907
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-02-26 01:46:35 +0900

    feat : line renderer로 polygon 표현

---
+++ OpenEnding_2022.3.8f1/Assets/DevOnly/AnimSystem/Polygon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Polygon : MonoBehaviour
+{
+    public int Sides { get; set; }
+    public float Radius { get; set; }
+    
+    public LineRenderer LineRenderer { get; private set; }
+    private void Awake()
+    {
+        Sides = 0;
+        Radius = 0f;
+        LineRenderer = gameObject.AddComponent<LineRenderer>();
+    }
+
+    public void Update()
+    {
+        if (Sides < 3 || Radius <= 0) return;
+    }
+}
