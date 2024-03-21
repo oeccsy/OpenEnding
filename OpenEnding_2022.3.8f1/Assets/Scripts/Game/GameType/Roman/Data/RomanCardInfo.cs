43eacf27886e02aade17a5c50e24ffd31ee8442a
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-22 00:56:36 +0900

    chore : create card info scriptable object

---
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/GameType/Roman/Data/RomanCardInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Game.GameType.Roman.Data
+{
+    [CreateAssetMenu(fileName = "CardInfoSO", menuName = "Scriptable Object/Roman/CardInfoSO")]
+    public class RomanCardInfo : ScriptableObject
+    {
+        public CardType cardType = CardType.None;
+        public string cardName;
+        [TextArea]
+        public string cardDesc;
+    }
+}
