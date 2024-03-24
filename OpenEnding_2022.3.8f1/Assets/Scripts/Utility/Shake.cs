9dcda29e9bd895458e36c080842a3a03d8cc1d33
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-24 22:51:39 +0900

    test : shake test

---
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Utility/Shake.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.GameType.Roman
+{
+    public class Shake : MonoBehaviour
+    {
+        public TextMeshProUGUI debugText;
+        public float threshold = 10f;
+        public delegate void ShakeHandler();
+
+        public event ShakeHandler OnEveryShake;
+        public event ShakeHandler OnNextShake;
+
+        private void Update()
+        {
+            var value = Input.acceleration.magnitude;
+            
+            if (value > 10)
+            {
+                OnEveryShake?.Invoke();
+                OnNextShake?.Invoke();
+                OnNextShake = null;
+                DebugCanvas.Instance.AddText(value);
+            }
+
+            debugText.text = value.ToString();
+        }
+    }
+}
