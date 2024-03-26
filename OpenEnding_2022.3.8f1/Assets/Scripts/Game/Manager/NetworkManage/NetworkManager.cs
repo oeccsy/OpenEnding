90217c0dfc80dbb97f97ce6a94f53e7cff9f570f
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-26 13:48:02 +0900

    refactor : NetworkManager.cs 정돈

--- OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/NetworkManage/NetworkManager.cs
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/Manager/NetworkManage/NetworkManager.cs
@@ -11,59 +11,34 @@ public partial class NetworkManager : Singleton<NetworkManager>
     public string networkName = "OpenEnding";
     public string clientName = "None";
     public Define.ConnectType connectType = Define.ConnectType.None;
+    
     public readonly List<Networking.NetworkDevice> connectedDeviceList = new List<Networking.NetworkDevice>();
-    public readonly Networking.NetworkDevice ownDeviceData = new Networking.NetworkDevice();  // Plugins Custom
+    public readonly Networking.NetworkDevice ownDeviceData = new Networking.NetworkDevice();
     
     [Header("Networking")]
-    [SerializeField]
-    private Networking networking = null;                                           // Plugins
-    [SerializeField]
-    private bool isWritingData = false;                                             // true이면 현재 Writing(Sending) 중 임을 나타내는 변수
-    [SerializeField]
-    public ServerSidePacketHandler serverSidePacketHandler;                                    
-    [SerializeField]
-    public ClientSidePacketHandler clientSidePacketHandler;
+    private Networking networking = null;
+    private bool isWritingData = false;
+
+#if UNITY_ANDROID || UNITY_IOS 
+
+    private void NetworkingInit()
+    {
+        if (networking != null) Destroy(networking);
+        
+        gameObject.AddComponent<Networking>();
+        networking.Initialize(null, null);
+    }
+
+    #region Server Side
+
+    public ServerSidePacketHandler serverSidePacketHandler;       
     
     public Action<Networking.NetworkDevice> OnDeviceReady = null;
     public Action<Networking.NetworkDevice> OnDeviceDisconnected = null;
     public Action<Networking.NetworkDevice, string, byte[]> OnReceiveDataFromClient = null;
     
-    public Action<string, string, byte[]> OnReceiveDataFromServer = null;
-
-#if UNITY_ANDROID || UNITY_IOS 
-    protected override void Awake()
-    {
-        base.Awake();
-        networking = GetComponent<Networking>();
-        NetworkingInit();
-    }
-
-    private void NetworkingInit()
-    {
-        if (networking == null) networking = gameObject.AddComponent<Networking>();
-        
-        networking.Initialize((error) =>
-        {
-            switch (error)
-            { 
-                case "Bluetooth LE Not Enabled" :
-                case "Bluetooth LE Not Powered Off" :
-                case "Bluetooth LE Not Available" :
-                case "Bluetooth LE Not Supported" :
-                    "error".Log();
-                    break;
-            }
-        }, (message) =>
-        {
-            //message.Log();
-        });
-    }
-
-    #region Server Side
-
     public void StartServer()
     {
-        "StartServer".Log();
         connectType = Define.ConnectType.Server;
         
         ownDeviceData.deviceListOrder = connectedDeviceList.Count;
@@ -71,19 +46,16 @@ public partial class NetworkManager : Singleton<NetworkManager>
         
         connectedDeviceList.Add(ownDeviceData);
 
-        networking.StartServer(networkName, (connectedDevice) =>
-        {
-            OnDeviceReady?.Invoke(connectedDevice);
-        }, (disconnectedDevice) =>
-        {
-            OnDeviceDisconnected?.Invoke(disconnectedDevice);
-        }, (device, characteristic, bytes) =>
-        {
-            OnReceiveDataFromClient?.Invoke(device, characteristic, bytes);
-        });
+        networking.StartServer
+        (
+            networkName, 
+            (connectedDevice) => OnDeviceReady?.Invoke(connectedDevice),
+            (disconnectedDevice) => OnDeviceDisconnected?.Invoke(disconnectedDevice),
+            (device, characteristic, bytes) => OnReceiveDataFromClient?.Invoke(device, characteristic, bytes)
+        );
     }
     
-    public IEnumerator SendBytesToTargetDevice(Networking.NetworkDevice targetDevice, Byte[] bytes)
+    public IEnumerator SendBytesToTargetDevice(Networking.NetworkDevice targetDevice, byte[] bytes)
     {
         if (connectType != Define.ConnectType.Server) yield break;
    
@@ -97,14 +69,11 @@ public partial class NetworkManager : Singleton<NetworkManager>
         }
         else
         {
-            networking.WriteDevice(targetDevice, bytes, () =>
-            {
-                isWritingData = false;
-            });    
+            networking.WriteDevice(targetDevice, bytes, () => isWritingData = false);    
         }
     }
 
-    public IEnumerator SendBytesToAllDevice(Byte[] bytes)
+    public IEnumerator SendBytesToAllDevice(byte[] bytes)
     {
         if (connectType != Define.ConnectType.Server) yield break;
         
@@ -120,108 +89,57 @@ public partial class NetworkManager : Singleton<NetworkManager>
             }
             else
             {
-                networking.WriteDevice(targetDevice, bytes, () =>
-                {
-                    isWritingData = false;
-                });     
+                networking.WriteDevice(targetDevice, bytes, () => isWritingData = false);     
             }
         }
     }
     
-    public IEnumerator SendBytesExceptOneDevice(Networking.NetworkDevice skipDevice, Byte[] bytes)
-    {
-        if (connectType != Define.ConnectType.Server) yield break;
-        yield return new WaitUntil(() => isWritingData);
-        isWritingData = true;
-        
-        foreach (var targetDevice in connectedDeviceList)
-        {
-            if (targetDevice == skipDevice) continue;
-            
-            if (targetDevice == ownDeviceData)
-            {
-                OnReceiveDataFromServer?.Invoke(null, null, bytes);
-            }
-            else
-            {
-                networking.WriteDevice(targetDevice, bytes, () =>
-                {
-                    
-                });    
-            }
-        }
-        
-        isWritingData = false;
-    }
-
     public void StopServer()
     {
-        networking.StopServer(() =>
-        {
-            "StopServer".Log();
-        });
+        networking.StopServer(null);
     }
 
     #endregion
     
     #region Client Side
     
+    public ClientSidePacketHandler clientSidePacketHandler;
+    
+    public Action<string, string, byte[]> OnReceiveDataFromServer = null;
+    
     public void StartClient()
     {
-        "Start Client".Log();
         connectType = Define.ConnectType.Client;
         
-        networking.StartClient(networkName, clientName, () =>
-        {
-            "Start Client Advertising".Log();
-        }, (client, characteristic, bytes) =>
-        {
-            OnReceiveDataFromServer?.Invoke(client, characteristic, bytes);
-        });
+        networking.StartClient
+        (
+            networkName,
+            clientName,
+            null,
+            (client, characteristic, bytes) => OnReceiveDataFromServer?.Invoke(client, characteristic, bytes)
+        );
     }
     
-    public IEnumerator SendBytesToServer(Byte[] bytes)
+    public IEnumerator SendBytesToServer(byte[] bytes)
     {
         yield return new WaitWhile(() => isWritingData);
         isWritingData = true;
 
-        switch (connectType)
+        if (connectType == Define.ConnectType.Client)
         {
-            case Define.ConnectType.Client:
-                networking.SendFromClient(bytes);
-                break;
-            case Define.ConnectType.Server:
-                OnReceiveDataFromClient?.Invoke(null, null, bytes);
-                break;
+            networking.SendFromClient(bytes);
+        }
+        else if (connectType == Define.ConnectType.Server)
+        {
+            OnReceiveDataFromClient?.Invoke(null, null, bytes);
         }
         
         isWritingData = false;
     }
     
-    public IEnumerator RequestSendBytesToTargetDevice(Networking.NetworkDevice targetDevice, Byte[] bytes)
-    {
-        yield return new WaitUntil(() => isWritingData);
-        isWritingData = true;
-
-        if (connectType == Define.ConnectType.Server)
-        {
-            yield return SendBytesToTargetDevice(targetDevice, bytes);
-        }
-        else
-        {
-            var request = new Byte[5]; // TODO
-            yield return SendBytesToServer(request);
-        }
-
-        isWritingData = false;
-    }
-
     public void StopClient()
     {
-        networking.StopClient(() =>
-        {
-            "StopClient".Log();
-        });
+        networking.StopClient(null);
     }
     
     #endregion
