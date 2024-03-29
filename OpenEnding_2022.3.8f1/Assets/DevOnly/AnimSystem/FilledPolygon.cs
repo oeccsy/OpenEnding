fe4ef4f40352e0a5d12b184d7a39254e3e41c584
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-03-29 21:27:33 +0900

    refactor : polygon 추상화

---
+++ OpenEnding_2022.3.8f1/Assets/DevOnly/AnimSystem/FilledPolygon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DevOnly.AnimSystem
+{
+    public class FilledPolygon : Polygon
+    {
+        public override void DrawPolygon(int sides, float radius)
+        {
+            Sides = sides;
+            Radius = radius;
+            
+            _polygonPoints = GetCircumferencePoints(sides, radius).ToArray();
+            _polygonTriangles = GetTriangles(_polygonPoints);
+        
+            _mesh.Clear();
+            _mesh.vertices = _polygonPoints;
+            _mesh.triangles = _polygonTriangles;
+        }
+
+        protected override int[] GetTriangles(Vector3[] points)
+        {
+            int triangleAmount = points.Length - 2;
+        
+            List<int> triangles = new List<int>();
+            for (int i = 0; i < triangleAmount; i++)
+            {
+                // Clockwise
+                triangles.Add(0);
+                triangles.Add(i+2);
+                triangles.Add(i+1);
+            }
+
+            return triangles.ToArray();
+        }
+    }
+}
--- OpenEnding_2022.3.8f1/Assets/DevOnly/AnimSystem/Polygon.cs
+++ OpenEnding_2022.3.8f1/Assets/DevOnly/AnimSystem/Polygon.cs
@@ -1,20 +1,69 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
-public class Polygon : MonoBehaviour
+namespace DevOnly.AnimSystem
 {
-    public int Sides { get; set; }
-    public float Radius { get; set; }
+    public abstract class Polygon : MonoBehaviour
+    {
+        protected MeshRenderer _meshRenderer;
+        protected MeshFilter _meshFilter;
     
-    public LineRenderer LineRenderer { get; private set; }
-    private void Awake()
-    {
-        Sides = 0;
-        Radius = 0f;
-        LineRenderer = gameObject.AddComponent<LineRenderer>();
-    }
+        protected Mesh _mesh;
+        protected Vector3[] _polygonPoints;
+        protected int[] _polygonTriangles;
 
-    public void Update()
-    {
-        if (Sides < 3 || Radius <= 0) return;
+        private int _sides = 3;
+        private float _radius = 1f;
+
+        public int Sides
+        {
+            get => _sides;
+            set
+            {
+                _sides = value;
+                DrawPolygon(_sides, _radius);
+            }
+        }
+
+        public float Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                DrawPolygon(_sides, _radius);
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            Radius = 0f;
+            _meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            _meshFilter = gameObject.AddComponent<MeshFilter>();
+            _meshFilter.mesh = _mesh = new Mesh();
+        }
+
+        public abstract void DrawPolygon(int sides, float radius);
+        
+        protected List<Vector3> GetCircumferencePoints(int sides, float radius)
+        {
+            if (sides < 3) return null;
+        
+            List<Vector3> points = new List<Vector3>();
+
+            for (int i = 0; i < sides; i++)
+            {
+                Vector3 point = Vector3.zero;
+                point.x = Mathf.Cos((1f / sides) * (2 * Mathf.PI) * i) * radius;
+                point.y = Mathf.Sin((1f / sides) * (2 * Mathf.PI) * i) * radius;
+                point.z = 0;
+            
+                points.Add(point);
+            }
+        
+            return points;
+        }
+
+        protected abstract int[] GetTriangles(Vector3[] points);
     }
 }
