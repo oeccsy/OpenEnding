4d68c5586f60c89471b1530439b2e5028991e584
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-05-27 01:47:49 +0900

    feat : OnBoardingUI Drag 기능

--- OpenEnding_2022.3.8f1/Assets/Scripts/Game/OnBoarding/OnBoardingUI.cs
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/OnBoarding/OnBoardingUI.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class OnBoardingUI : MonoBehaviour
+namespace Game.OnBoarding
 {
-    // Start is called before the first frame update
-    void Start()
+    public class OnBoardingUI : MonoBehaviour
     {
-        
-    }
+        public int contentIndex = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
+        [SerializeField]
+        private ScrollRect scrollRect;
+        [SerializeField]
+        private RectTransform contentRect;
+        [SerializeField]
+        private DragEventHandler contentDragEventHandler;
+        [SerializeField]
+        private Image[] progressDots;
+    
+        private void Awake()
+        {
+            contentDragEventHandler.OnDragEnd += SnapContent;
+            contentDragEventHandler.OnDragEnd += UpdateProgressDot;
+        }
+    
+        private void SnapContent(PointerEventData eventData)
+        {
+            scrollRect.velocity = Vector2.zero;
+            scrollRect.enabled = false;
+            contentDragEventHandler.dragable = false;
         
+            var targetIndex = (int)(-1 * contentRect.anchoredPosition.x + 540) / 1080;
+            Debug.Log(targetIndex);
+        
+            Sequence sequence = DOTween.Sequence();
+            sequence
+                .Append(contentRect.DOAnchorPos(new Vector2(-1080, 0) * targetIndex, 0.3f, true))
+                .AppendCallback(() => scrollRect.enabled = true)
+                .AppendCallback(() => contentDragEventHandler.dragable = true);
+        }
+    
+        private void UpdateProgressDot(PointerEventData eventData)
+        {
+            var targetIndex = (int)(-1 * contentRect.anchoredPosition.x + 540) / 1080;
+            var prevDot = progressDots[contentIndex];
+            var nextDot = progressDots[targetIndex];
+
+            if (targetIndex == contentIndex) return;
+        
+            contentIndex = targetIndex;
+
+            Sequence sequence = DOTween.Sequence();
+            sequence
+                .Append(prevDot.DOColor(ColorPalette.GetColor(ColorPalette.ColorName.ProgressDotDefault), 0.3f))
+                .Join(nextDot.transform.DOPunchScale(Vector3.one, 0.3f))
+                .Join(nextDot.DOColor(ColorPalette.GetColor(ColorPalette.ColorName.ProgressDotActive), 0.3f));
+        }
     }
 }
