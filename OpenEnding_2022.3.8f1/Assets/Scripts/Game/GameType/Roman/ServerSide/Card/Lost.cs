d177ce92fa79a464524c362094f61ee1ed9eef4d
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-06-04 10:29:14 +0900

    feat : lost card

---
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Game/GameType/Roman/ServerSide/Card/Lost.cs
@@ -0,0 +1,29 @@
+﻿using Game.GameType.Roman.ServerSide.CardBase;
+using Game.Manager.GameManage;
+using UnityEngine;
+
+namespace Game.GameType.Roman.ServerSide.Card
+{
+    public class Lost : RomanCard, IFlipAbility
+    {
+        public Lost()
+        {
+            cardType = CardType.F;
+        }
+        
+        public void FlipAbility()
+        {
+            var gameMode = GameManager.Instance.GameMode as RomanGameMode;
+            if (gameMode == null) return;
+            
+            var growableCards = gameMode.cardContainer.GetCards<IGrowable>();
+            if (growableCards.Count == 0) return;
+            
+            int randomIndex = Random.Range(0, growableCards.Count);
+            growableCards[randomIndex].Regress();
+        }
+        
+        public override void OnEnterField() {}
+        public override void OnExitField() {}
+    }
+}
