829e4d8acb6ec95ccef5847f138f6b615e530f6e
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-08-08 21:30:57 +0900

    feat : AndroidConnection

---
+++ OpenEnding_2022.3.8f1/Assets/Scripts/Connection/AndroidConnection.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidConnection : Singleton<AndroidConnection>
+{
+  private AndroidJavaClass _androidUtilsClass;
+  private AndroidJavaObject _androidUtilsInstance;
+
+  private AndroidJavaClass _bluetoothLEUtilsClass;
+  private AndroidJavaObject _bluetoothLEUtilsInstance;
+
+  private AndroidJavaClass _centralClass;
+  private AndroidJavaObject _centralInstance;
+
+  private AndroidJavaClass _peripheralClass;
+  private AndroidJavaObject _peripheralInstance;
+
+  public delegate void BluetoothDataHandler(byte[] data);
+  public event BluetoothDataHandler OnBluetoothDataReceive;
+
+  protected override void Awake()
+  {
+    base.Awake();
+    DontDestroyOnLoad(gameObject);
+
+    _androidUtilsClass = new AndroidJavaClass("com.oeccsy.openending_ble.AndroidUtils");
+    _androidUtilsInstance = _androidUtilsClass.CallStatic<AndroidJavaObject>("getInstance");
+
+    _bluetoothLEUtilsClass = new AndroidJavaClass("com.oeccsy.openending_ble.BluetoothLEUtils");
+    _bluetoothLEUtilsInstance = _bluetoothLEUtilsClass.CallStatic<AndroidJavaObject>("getInstance");
+
+    _centralClass = new AndroidJavaClass("com.oeccsy.openending_ble.Central");
+    _centralInstance = _centralClass.CallStatic<AndroidJavaObject>("getInstance");
+
+    _peripheralClass = new AndroidJavaClass("com.oeccsy.openending_ble.Peripheral");
+    _peripheralInstance = _peripheralClass.CallStatic<AndroidJavaObject>("getInstance");
+  }
+
+  public void Toast(string text)
+  {
+    _androidUtilsClass.CallStatic("toast", text);
+  }
+
+  public void IsBluetoothLEFeatureExist()
+  {
+    Toast(_bluetoothLEUtilsInstance.Call<bool>("isBluetoothLEFeatureExist").ToString());
+  }
+
+  public void HasBluetoothLEPermissions()
+  {
+    Toast(_bluetoothLEUtilsInstance.Call<bool>("hasBluetoothLEPermissions").ToString());
+  }
+
+  public void IsBluetoothLEEnable()
+  {
+    Toast(_bluetoothLEUtilsInstance.Call<bool>("isBluetoothLEEnable").ToString());
+  }
+
+  public void RequestPermissions()
+  {
+    _bluetoothLEUtilsInstance.Call("requestPermissions");
+  }
+
+  public void InitCentralBluetoothSystem()
+  {
+    _centralInstance.Call("initBluetoothSystem");
+  }
+
+  public void StartScanning()
+  {
+    _centralInstance.Call("startScanning");
+  }
+
+  public void StopScanning()
+  {
+    _centralInstance.Call("stopScanning");
+  }
+
+  public void Write(string deviceName, byte[] data)
+  {
+    _centralInstance.Call("write", deviceName, data);
+  }
+
+
+  public void InitPeripheralBluetoothSystem()
+  {
+    _peripheralInstance.Call("initBluetoothSystem");
+  }
+
+  public void StartAdvertising(string deviceName)
+  {
+    _peripheralInstance.Call("startAdvertising", deviceName);
+  }
+
+  public void StopAdvertising()
+  {
+    _peripheralInstance.Call("stopAdvertising");
+  }
+
+  public void StartServer()
+  {
+    _peripheralInstance.Call("startServer");
+  }
+
+  public void StopServer()
+  {
+    _peripheralInstance.Call("stopServer");
+  }
+
+  public void Indicate(byte[] data)
+  {
+    _peripheralInstance.Call("indicate", data);
+  }
+
+
+  public void OnDataReceive(string encodedData)
+  {
+    byte[] bytes = System.Convert.FromBase64String(encodedData);
+    OnBluetoothDataReceive?.Invoke(bytes);
+  }
+}
