ca5b1e91e40687a0a81821f0f69c1b973ea48ad0
YunSeong <77562357+oeccsy@users.noreply.github.com>
2024-08-10 08:38:16 +0900

    test : reflection test

---
+++ OpenEnding_2022.3.8f1/Assets/DevOnly/Test/ReflectionTest.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEngine;
+
+public class ReflectionTest : MonoBehaviour
+{
+    protected Dictionary<Type, object> _instanceDict = new Dictionary<Type, object>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        _instanceDict.Add(typeof(ReflectionTest), this);
+        Test();
+    }
+
+    public void Test()
+    {
+        Type type = Type.GetType("ReflectionTest");
+        byte[] param = {19, 23};
+
+        object[] parameters = new object[param.Length];
+        for(int i=0; i<parameters.Length; i++)
+        {
+            parameters[i] = param[i];
+        }
+            
+        object instance = _instanceDict[type];
+        MethodInfo methodInfo = instance.GetType().GetMethod("Log");
+        methodInfo.Invoke(instance, parameters);
+
+        Debug.Log(instance.GetType().FullName);
+    }
+
+    public void Log(int a, int b)
+    {
+        Debug.Log($"TestTestTest {a} {b}");
+    }
+}
